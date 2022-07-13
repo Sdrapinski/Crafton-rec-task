f455d598d40df2e54846491785751fa22a10fb9c
Sdrapinski <szymondrapinski@gmail.com>
2022-07-13 23:53:05 +0200

    changed sliders margin top

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,12 @@
 const burgerShow = () => {
   const links = document.querySelector(".navbar__links");
+  const slider = document.querySelector(".slider__navbar");
   const burger = document.querySelector(".burger");
+
   burger.addEventListener("click", () => {
     links.classList.toggle("blok");
+    slider.classList.add("sticky");
+    burger.classList.toggle("close");
   });
 };
 
